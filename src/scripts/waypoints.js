ed338acc8520ab892f292199ecd4eca571f24dd9
Umair Ali <umairali17@hotmail.co.uk>
2016-10-11 22:07:08 +0100

    Installed Git Modules (All)

--- src/scripts/waypoints.js
+++ src/scripts/waypoints.js
@@ -5,23 +5,27 @@ var $secondAnimate = $('.rightBox');
 $(document).ready(function() {
     //Animate First Box Coming From Left
     $firstAnimate.css('opacity', 0);
+    $(".img-right").css('opacity', 0);
 	  $firstAnimate.waypoint(function(direction) {
       if (direction === 'down') {
          $(this.element).addClass('fadeInLeft');
+          $(".img-right").addClass('fadeIn');
       }
     }, {
-      offset: '20%'
+      offset: '35%'
     });
     
     
     //Animate Second Box coming from right
     $secondAnimate.css('opacity', 0);
+     $(".img-left").css('opacity', 0);
   	$secondAnimate.waypoint(function(direction) {
       if (direction === 'down') {
-         $(this.element).addClass('fadeInRight delay-05s');
+         $(this.element).addClass('fadeInRight');
+        $(".img-left").addClass('fadeIn');
       }
     }, {
-      offset: '20%'
+      offset: '35%'
     });
 
 
