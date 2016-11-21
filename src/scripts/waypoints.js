ad2dbc897f64ca9df2c9f9a3f21c342a87133122
U.Ali U1351864 <U1351864@unimail.hud.ac.uk>
2016-11-21 16:05:23 +0000

    Fixed Waypoints Audio

--- src/scripts/waypoints.js
+++ src/scripts/waypoints.js
@@ -1,7 +1,7 @@
 //First Waypoint
 var $firstAnimate = $('.leftBox');
 var $secondAnimate = $('.rightBox');
-
+var $audioWaypoint = $('.audioBox');
 $(document).ready(function() {
     //Animate First Box Coming From Left
     $firstAnimate.css('opacity', 0);
@@ -23,6 +23,20 @@ $(document).ready(function() {
       if (direction === 'down') {
          $(this.element).addClass('fadeInRight');
         $(".img-left").addClass('fadeIn');
+      }
+    }, {
+      offset: '35%'
+    });
+    
+    
+    
+    $audioWaypoint.waypoint(function(direction) 
+    {
+      if (direction === 'down') {
+       //get a child div that's equal to .music which is in .audioBox
+       var $rowMusic = $(this.element).children(".music").get(0);
+       $rowMusic.play();
+       console.log($rowMusic);
       }
     }, {
       offset: '35%'
