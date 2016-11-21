6f52e4a4b7eff3ab8213d5155b138824da45db61
U.Ali U1351864 <U1351864@unimail.hud.ac.uk>
2016-11-21 15:00:04 +0000

    Fixed Gulp and Started on Design

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var runSequence = require('run-sequence');
 
 //Set up the server
 gulp.task('sass', function() {
-  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in src/scss
     .pipe(sass())
     .pipe(gulp.dest('src/styles'))
     .pipe(browserSync.reload({
@@ -20,10 +20,16 @@ gulp.task('sass', function() {
     }))
 });
 
-gulp.task('serve', function() {
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+
+gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: 'src'
     },
   })
 })
@@ -33,31 +39,31 @@ gulp.task('useref', function(){
   return gulp.src('src/*.html')
     .pipe(useref())
     // Minifies only if it's a JavaScript file
-    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.scripts', uglify()))
     // Minifies only if it's a CSS file
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('watch', ['serve', 'sass'], function (){
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('src/scss/**/*.scss', ['sass']); 
   // Other watchers
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('src/*.html', browserSync.reload); 
-  gulp.watch('src/scripts/**/*.js', browserSync.reload); 
+  gulp.watch('src/scripts/**/*.scripts', browserSync.reload); 
 });
 
 //the following will run all the below tasks in the order they are written. items in [] will run in parallel
 gulp.task('build', function (callback) {
-  runSequence(['sass', 'useref'],
+  runSequence(['sass', 'fonts', /*'images',*/ 'useref'],
     callback
   )
 })
 
 //The following will run the tasks of compiling the sass, running the server and then running the watch tasks when you run "gulp" in the cli
 gulp.task('default', function (callback) {
-  runSequence('build', ['serve', 'watch'],
+  runSequence('build', ['browserSync', 'watch'],
     callback
   )
 })
-
